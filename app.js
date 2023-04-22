488a9fdd13dd1309fde298a06533de467d3610f5
lohchanhin <chanhin17@gmail.com>
2023-04-22 15:45:31 +0800

    4

--- app.js
+++ app.js
@@ -48,6 +48,42 @@ const fetchStockData = async (ticker) => {
   }
 };
 
+const fetchStockHistoryData = async(ticker)=>{
+    try {
+        // 設置日期範圍為過去一個月
+        const today = new Date();
+        const oneMonthAgo = new Date();
+        oneMonthAgo.setMonth(today.getMonth() - 1);
+    
+        // 將日期轉換為 yyyy-mm-dd 格式
+        const formatDate = (date) => {
+          const year = date.getFullYear();
+          const month = String(date.getMonth() + 1).padStart(2, '0');
+          const day = String(date.getDate()).padStart(2, '0');
+    
+          return `${year}-${month}-${day}`;
+        };
+    
+        const fromDate = formatDate(oneMonthAgo);
+        const toDate = formatDate(today);
+    
+        // 使用 yfinance 獲取股票的歷史市場數據
+        const historicalData = await yahooFinance.historical({
+          symbol: ticker,
+          from: fromDate,
+          to: toDate,
+        });
+    
+        console.log(`${ticker} historical data:`);
+        console.log(historicalData);
+    
+        return historicalData;
+      } catch (error) {
+        console.error(`Error fetching data for ${ticker}:`, error);
+      }
+}
+
+
 // 定义事件处理函数
 async function handleEvent(event) {
   // 如果事件类型不是消息或消息类型不是文本，则忽略
@@ -57,32 +93,8 @@ async function handleEvent(event) {
 
   // 检查前四个字符是否是 "分析k線"
   if (event.message.text.slice(0, 4) === "分析k線") {
-    // 設置日期範圍為過去一個月
-    const today = new Date();
-    const oneMonthAgo = new Date();
-    oneMonthAgo.setMonth(today.getMonth() - 1);
-
-    // 將日期轉換為 yyyy-mm-dd 格式
-    const formatDate = (date) => {
-      const year = date.getFullYear();
-      const month = String(date.getMonth() + 1).padStart(2, '0');
-      const day = String(date.getDate()).padStart(2, '0');
-
-      return `${year}-${month}-${day}`;
-    };
-
-    const fromDate = formatDate(oneMonthAgo);
-    const toDate = formatDate(today);
-
-    // 修改 Symbol => symbol, 变量名 ticker => stockCode
-    const target = await yahooFinance.historical({
-      symbol: event.message.text.slice(4),
-      from: fromDate,
-      to: toDate,
-    });
- 
-
-    client.replyMessage(event.replyToken, target);
+    const stockCode = event.message.text.slice(4).replace(/\s+/g, '');
+    const target = fetchStockHistoryData(stockCode)
 
     const configuration = new Configuration({ apiKey: apiKey2 });
     const openai = new OpenAIApi(configuration);
@@ -95,7 +107,7 @@ async function handleEvent(event) {
             },
             {
             role: "user",
-            content: "Analyze the past month's K-line data for the following stock: " + JSON.stringify(target)
+            content: "根據過去一個月K線資料以技術指標分析是否適合購買: " + JSON.stringify(target)
             }
         ],
         max_tokens: 2000,
@@ -125,7 +137,7 @@ async function handleEvent(event) {
               },
               {
                 role: "user",
-                content: "Analyze the financial report for the stock with code: " + target
+                content: "根據財報為該公司進行評分,滿分10分: " + target
               }
             ],
             max_tokens: 2000,
@@ -140,11 +152,13 @@ async function handleEvent(event) {
           // 使用 LINE API 发送消息
           return client.replyMessage(event.replyToken, reply);
         }
-      }
+}
       
-      // 监听端口
-      const port = process.env.PORT || 3000;
-      app.listen(port, () => {
-        console.log(`listening on ${port}`);
-      });
+// 监听端口
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+console.log(`listening on ${port}`);
+});
       
+// console.log(fetchStockData('2330.TW'))
+// console.log(fetchStockHistoryData('2330.TW'))
